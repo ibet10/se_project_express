57568ffbfa53ffbca7bc2f4b5b57e4ccd2cfcfd3
ibet1 <contact.ibet10raya@gmail.com>
2025-01-17 17:21:42 -0500

    S12 - error update 3

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require("express");
-const { PORT = 3001 } = process.env;
+
 const mongoose = require("mongoose");
+
+const { PORT = 3001 } = process.env;
 
 mongoose.set("strictQuery", true); // Set because of: (node:14092) [MONGOOSE] DeprecationWarning: Mongoose: the `strictQuery` option will be switched back to `false` by default in Mongoose 7.
 // Use `mongoose.set('strictQuery', false);` if you want to prepare for this change. Or use `mongoose.set('strictQuery', true);` to suppress this warning.
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133",
+  };
+  next();
+});
 
 const app = express();
 
