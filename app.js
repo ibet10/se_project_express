9f8187ef50782e00441ccc80dfb5d3033c7453b2
ibet1 <contact.ibet10raya@gmail.com>
2025-01-17 17:24:28 -0500

    S12 - update ESLint error

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const { PORT = 3001 } = process.env;
 mongoose.set("strictQuery", true); // Set because of: (node:14092) [MONGOOSE] DeprecationWarning: Mongoose: the `strictQuery` option will be switched back to `false` by default in Mongoose 7.
 // Use `mongoose.set('strictQuery', false);` if you want to prepare for this change. Or use `mongoose.set('strictQuery', true);` to suppress this warning.
 
+const app = express();
+
 app.use((req, res, next) => {
   req.user = {
     _id: "5d8b8592978f8bd833ca8133",
   };
   next();
 });
-
-const app = express();
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
