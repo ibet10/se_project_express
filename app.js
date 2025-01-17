2fcdaaeb6f07b9ed758958984395aece0a6eae2b
ibet1 <contact.ibet10raya@gmail.com>
2025-01-17 15:47:37 -0500

    S12 - error

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const { PORT = 3001 } = process.env;
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true); //Set because of: (node:14092) [MONGOOSE] DeprecationWarning: Mongoose: the `strictQuery` option will be switched back to `false` by default in Mongoose 7.
+// Use `mongoose.set('strictQuery', false);` if you want to prepare for this change. Or use `mongoose.set('strictQuery', true);` to suppress this warning.
 
 const app = express();
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch(console.error);
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
